0f7a6e25fa6ea674031d916747530ff837908440
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-04-30 14:33:13 -0400

    added the prompts and created a function to create a response to those prompts

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const db = require('./db/connection');
 
 
@@ -17,6 +18,49 @@ app.get('/', (req, res) => {
       message: 'Hello World'
     });
 });
+
+//THEN I am presented with the following options: view all departments, view all roles, view all employees, 
+// add a department, add a role, add an employee, and update an employee role
+
+const directory = [
+    {
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    }
+]
+
+// ======== Prompt ========
+function prompt() {
+    inquirer.prompt(directory)
+        .then(function(val) {
+            if (val.options === 'View all departments') {
+                viewDepartments();
+            } else if (val.options === 'View all roles') {
+                viewRoles();
+            } else if (val.options === 'View all employees') {
+                viewEmployees();
+            } else if (val.options === 'Add a department') {
+                addDepartment();
+            } else if (val.options === 'Add a role') {
+                addRole();
+            } else if (val.options === 'Add an employee') {
+                addEmployee();
+            } else if (val.options === 'Update an employee role') {
+                updateEmployee();
+            }
+        })
+}
+
 
 app.use((req, res) => {
   res.status(404).end();
